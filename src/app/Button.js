bbe2ac4d62790d2e999a1dc9ba327d26f565fa35
Pavlik07 <pavelmos0707@gmail.com>
2023-11-28 15:03:45 +0200

    deleted some comments

--- src/app/Button.js
+++ src/app/Button.js
@@ -1,60 +1,6 @@
 import React from 'react'
-import * as math from 'mathjs'
 
 function Button(props) {
-    // function handleNumberClick (num) {
-    //     setInputNum(inputNum.concat(num));
-    //     setUserWindow(userWindow.concat(num));
-    // }
-    // function handlePlusClick () {
-    //     setInputNum('');
-    //     if(last === '+' || last === '-' || last === '*' || last === '/' || last === '='  || last === ')') {
-    //       return 0
-    //     }
-    //     else
-    //     setResult(math.evaluate(userWindow));
-    //     setUserWindow(userWindow.concat('+'));
-    // }
-    // function handleMinusClick () {
-    //     setInputNum('')
-    //     if(last === '+' || last === '-' || last === '*' || last === '/' || last === '='  || last === ')') {
-    //       return 0
-    //     }
-    //     else
-    //     setResult(math.evaluate(userWindow))
-    //     setUserWindow(userWindow.concat('-'));
-    // }
-    // function handleEqualsClick () {
-    //     setInputNum('')
-    //     if(last === '+' || last === '-' || last === '*' || last === '/' || last === '='  || last === ')') {
-    //       return 0
-    //     }
-    //     else
-    //     setResult(math.evaluate(userWindow))
-    // }
-    // function handleMultiplyClick () {
-    //     setInputNum('');
-    //     if(last === '+' || last === '-' || last === '*' || last === '/' || last === '='  || last === ')') {
-    //       return 0
-    //     }
-    //     else
-    //     setResult(math.evaluate(userWindow));
-    //     setUserWindow(`(${userWindow})*`);
-    // }
-    // function handleDevideClick () {
-    //     setInputNum('');
-    //     if(last === '+' || last === '-' || last === '*' || last === '/' || last === '='  || last === ')') {
-    //       return 0
-    //     }
-    //     else
-    //     setResult(math.evaluate(userWindow));
-    //     setUserWindow(`(${userWindow})/`);
-    // }
-    // function handleDeleteClick () {
-    //     setInputNum('');
-    //     setResult('');
-    //     setUserWindow('');
-    // }
     return (
         <div className='allButtons'>
             <button onClick={() => props.handleNumberClick(1)}>1</button>
